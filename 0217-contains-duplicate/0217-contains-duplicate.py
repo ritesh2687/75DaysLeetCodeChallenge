c5b7e5f4a83e5926e78fb0f03b21beab670b14ae
RITESH PRAJAPATI <149570198+ritesh2687@users.noreply.github.com>
2026-03-13 12:00:09 +0530

    Time: 43 ms (14.36%), Space: 24.4 MB (91.62%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        freq={}
+        for num in nums:
+            if num in freq:
+                freq[num]+=1
+            else:
+                freq[num]=1
+        for key in freq:
+            if freq[key]>1:
+                return True
+            
+        return False
+
+        
