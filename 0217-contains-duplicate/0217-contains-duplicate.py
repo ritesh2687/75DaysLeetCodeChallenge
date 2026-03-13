34ae55d64e1c353708ea2f7bc92a379881433601
RITESH PRAJAPATI <149570198+ritesh2687@users.noreply.github.com>
2026-03-13 12:14:18 +0530

    Time: 16 ms (70.3%), Space: 26 MB (48.29%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -3,17 +3,24 @@ class Solution(object):
         """
         :type nums: List[int]
         :rtype: bool
-        """
-        freq={}
-        for num in nums:
-            if num in freq:
-                freq[num]+=1
-            else:
-                freq[num]=1
-        for key in freq:
-            if freq[key]>1:
-                return True
+        # """
+        # freq={}
+        # for num in nums:
+        #     if num in freq:
+        #         freq[num]+=1
+        #     else:
+        #         freq[num]=1
+        # for key in freq:
+        #     if freq[key]>1:
+        #         return True
             
+        # return False
+
+        se =set()
+        for num in nums:
+            if num in se:
+                return True
+            se.add(num)
         return False
 
         
