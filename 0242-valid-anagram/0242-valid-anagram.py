c055cf15a7dd3492045fe939fcc5d5ba17b1c75c
RITESH PRAJAPATI <149570198+ritesh2687@users.noreply.github.com>
2026-03-14 19:57:33 +0530

    Time: 19 ms (66.71%), Space: 12.6 MB (81.36%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -5,6 +5,21 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        if(sorted(s)==sorted(t)):
-            return True
-        return False
+        # if(sorted(s)==sorted(t)):
+        #     return True
+        # return False
+
+        if len(s) != len(t):
+            return False
+
+        check={}
+
+        for c in s:
+            check[c]=check.get(c,0)+1
+        
+        for c in t:
+            if c not in check or check[c]==0:
+                return False
+            check[c] -=1
+        
+        return True
