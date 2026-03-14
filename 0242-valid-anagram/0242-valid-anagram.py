a72df259882a890e66473012afc3ccf4965a655a
RITESH PRAJAPATI <149570198+ritesh2687@users.noreply.github.com>
2026-03-14 20:02:10 +0530

    Time: 15 ms (87.56%), Space: 12.6 MB (67.11%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -9,28 +9,20 @@ class Solution(object):
         #     return True
         # return False
 
-        # if len(s) != len(t):
-        #     return False
-
-        # check={}
-
-        # for c in s:
-        #     check[c]=check.get(c,0)+1
-        
-        # for c in t:
-        #     if c not in check or check[c]==0:
-        #         return False
-        #     check[c] -=1
-        
-        # return True
-
-       
         if len(s) != len(t):
             return False
-    
-        count = [0]*26
-        for a,b in zip(s,t):
-            count[ord(a)-97]+=1
-            count[ord(b)-97]-=1
+
+        check={}
+
+        for c in s:
+            check[c]=check.get(c,0)+1
         
-        return count == [0]*26
+        for c in t:
+            if c not in check or check[c]==0:
+                return False
+            check[c] -=1
+        
+        return True
+
+       
+    
