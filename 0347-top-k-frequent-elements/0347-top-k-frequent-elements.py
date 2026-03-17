2436c6f2417af6d915dd6fe9b6e016d00203a238
RITESH PRAJAPATI <149570198+ritesh2687@users.noreply.github.com>
2026-03-17 17:45:29 +0530

    Time: 4 ms (94.49%), Space: 14.3 MB (69.62%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -12,6 +12,6 @@ class Solution(object):
 
         sorted_items = sorted(freq.items(), key=lambda x: x[1], reverse=True)
 
-        result = [num for num, count in sorted_items[:k]]
+        result = [item[0] for item in sorted_items[:k]]
 
         return result
