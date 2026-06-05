ae82beba139a78806a67de822b32c7c435dcbccd
RITESH PRAJAPATI <149570198+ritesh2687@users.noreply.github.com>
2026-06-05 10:52:29 +0530

    Time: 282 ms (86.75%), Space: 0B (100%) - LeetHub

--- 0595-big-countries/0595-big-countries.sql
+++ 0595-big-countries/0595-big-countries.sql
@@ -1,7 +1,4 @@
 # Write your MySQL query statement bel
 #SELECT POPULATION,gdp from world;
 #select name,population,area from world where population>25000000 and area>3000000;
-SELECT name,population,area 
-FROM world 
-WHERE population >= 25000000 
-or area >= 3000000;
+SELECT name,population,area FROM world WHERE population >= 25000000 or area >= 3000000;
