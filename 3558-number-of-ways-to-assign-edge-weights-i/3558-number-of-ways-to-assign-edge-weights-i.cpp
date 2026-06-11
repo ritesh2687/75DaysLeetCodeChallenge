f3db05cabf8af7b1619f0d9ebd86522dc96cb6f7
RITESH PRAJAPATI <149570198+ritesh2687@users.noreply.github.com>
2026-06-11 23:00:25 +0530

    Time: 499 ms (18.79%), Space: 395.4 MB (13.42%) - LeetHub

---
+++ 3558-number-of-ways-to-assign-edge-weights-i/3558-number-of-ways-to-assign-edge-weights-i.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int M=1e9+7;
+    typedef long long ll;
+    ll power(ll base,ll exponent){
+        if(exponent==0){
+            return 1;
+        }
+        ll half=power(base,exponent/2);
+        ll result=(half*half) % M;
+
+        if(exponent % 2 ==1){
+            result=(result*base)%M;
+        }
+        return result;
+    }
+
+    int getMaxDepth(unordered_map<int,vector<int>>& adj,int node,int parent){
+        int depth=0;
+        for(int &ngbd: adj[node]){
+            if(ngbd==parent) continue;
+
+            depth=max(depth,getMaxDepth(adj,ngbd,node)+1);
+
+        }
+        return depth;
+    }
+
+    int assignEdgeWeights(vector<vector<int>>& edges) {
+
+
+     unordered_map<int,vector<int>> adj;
+
+     for(auto &edge :edges){
+        int u=edge[0];
+        int v=edge[1];
+
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+
+     }  
+
+     int d= getMaxDepth(adj,1,-1); 
+     return power(2,d-1);
+    }
+};
