11fefb317db05be0a5372e36ec903e056c7c5487
RITESH PRAJAPATI <149570198+ritesh2687@users.noreply.github.com>
2026-06-12 14:59:29 +0530

    Time: 4 ms (14.62%), Space: 10.3 MB (24.37%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string cleaned="";
+        
+        for(char c:s){
+            if(isalnum(c))
+            cleaned+=tolower(c);
+        }
+        int l=0,r=cleaned.length()-1;
+
+
+        while(l<r){
+            if(cleaned[l]!=cleaned[r]){
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+};
