b107a1dd71f9fbc8caa290ba40846d78a6c854c0
RITESH PRAJAPATI <149570198+ritesh2687@users.noreply.github.com>
2026-06-16 23:42:31 +0530

    Time: 6 ms (39.02%), Space: 39.6 MB (87.5%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string processStr(string s) {
+        
+        string result;
+
+        for (char c : s) {
+            if (islower(c)) {
+                result.push_back(c);
+            } 
+            else if (c == '*') {
+                if (!result.empty()) {
+                    result.pop_back();
+                }
+            } 
+            else if (c == '#') {
+                result += result;
+            } 
+            else if (c == '%') {
+                reverse(result.begin(), result.end());
+            }
+        }
+
+        return result;    
+    }
+};
