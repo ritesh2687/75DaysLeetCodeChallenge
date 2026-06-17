3489ab42bd68fff20784412631c4d88299bf66ce
RITESH PRAJAPATI <149570198+ritesh2687@users.noreply.github.com>
2026-06-17 21:35:33 +0530

    Time: 23 ms (77.01%), Space: 24.1 MB (28.74%) - LeetHub

---
+++ 3614-process-string-with-special-operations-ii/3614-process-string-with-special-operations-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    char processStr(string s, long long k) {
+        long long len = 0;
+
+        for (char c : s) {
+            if (c == '*') {
+                len = max(0LL, len - 1);
+            } else if (c == '#') {
+                len <<= 1;
+            } else if (c != '%') {
+                ++len;
+            }
+        }
+
+        if (k >= len) return '.';
+
+        for (int i = s.size() - 1; i >= 0; --i) {
+            char c = s[i];
+
+            if (c == '*') {
+                ++len;
+            } else if (c == '#') {
+                len /= 2;
+                if (k >= len) k -= len;
+            } else if (c == '%') {
+                k = len - 1 - k;
+            } else {
+                --len;
+                if (k == len) return c;
+            }
+        }
+
+        return '.';
+    }
+};
