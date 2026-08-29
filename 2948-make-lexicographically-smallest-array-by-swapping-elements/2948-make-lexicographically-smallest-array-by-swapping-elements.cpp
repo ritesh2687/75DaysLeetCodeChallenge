685c68568a80b43e6db3e145dfe84aa853c108cc
RITESH PRAJAPATI <149570198+ritesh2687@users.noreply.github.com>
2026-08-29 21:14:47 +0530

    Time: 183 ms (70.7%), Space: 184.2 MB (58.31%) - LeetHub

--- 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.cpp
+++ 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.cpp
@@ -1,40 +1,37 @@
-#include <vector>
-#include <algorithm>
-#include <unordered_map>
-#include <deque>
-
 class Solution {
 public:
-    std::vector<int> lexicographicallySmallestArray(std::vector<int>& nums, int limit) {
+    vector<int> lexicographicallySmallestArray(vector<int>& nums, int limit) {
         int n = nums.size();
-        std::vector<std::pair<int, int>> sorted_nums(n);
+        vector<pair<int, int>> sorted_nums(n);
         for (int i = 0; i < n; ++i) {
             sorted_nums[i] = {nums[i], i};
         }
         
-        std::sort(sorted_nums.begin(), sorted_nums.end());
+        sort(sorted_nums.begin(), sorted_nums.end());
         
-        std::vector<std::deque<int>> groups;
-        std::unordered_map<int, int> num_to_group;
+        vector<vector<int>> group_vals;
+        vector<vector<int>> group_indices;
         
-        int group_idx = 0;
-        groups.push_back({sorted_nums[0].first});
-        num_to_group[sorted_nums[0].first] = group_idx;
-        
-        for (int i = 1; i < n; ++i) {
-            if (sorted_nums[i].first - sorted_nums[i - 1].first > limit) {
-                group_idx++;
-                groups.push_back({});
+        for (int i = 0; i < n; ++i) {
+            if (group_vals.empty() || sorted_nums[i].first - group_vals.back().back() > limit) {
+                group_vals.push_back({sorted_nums[i].first});
+                group_indices.push_back({sorted_nums[i].second});
+            } else {
+                group_vals.back().push_back(sorted_nums[i].first);
+                group_indices.back().push_back(sorted_nums[i].second);
             }
-            groups[group_idx].push_back(sorted_nums[i].first);
-            num_to_group[sorted_nums[i].first] = group_idx;
         }
         
-        std::vector<int> result(n);
-        for (int i = 0; i < n; ++i) {
-            int g_idx = num_to_group[nums[i]];
-            result[i] = groups[g_idx].front();
-            groups[g_idx].pop_front();
+        vector<int> result(n);
+        for (size_t i = 0; i < group_vals.size(); ++i) {
+            auto& indices = group_indices[i];
+            auto& vals = group_vals[i];
+            
+            sort(indices.begin(), indices.end());
+            
+            for (size_t j = 0; j < indices.size(); ++j) {
+                result[indices[j]] = vals[j];
+            }
         }
         
         return result;
