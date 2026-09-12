6cacba59e1f5f078783049f61f40d86f287754e8
RITESH PRAJAPATI <149570198+ritesh2687@users.noreply.github.com>
2026-09-12 11:12:41 +0530

    Time: 0 ms (100%), Space: 14.5 MB (8.66%) - LeetHub

--- 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.cpp
+++ 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     int findNumbers(vector<int>& nums) {
-        int count=0;
-        for(int n:nums){
-            if(to_string(n).length()%2==0) count++;
-        }
-
-        return count;
+    int count=0;
+    for(int n:nums){
+        if (to_string(n).length()%2==0) count++;
+    }
+    return count;
     }
 };
